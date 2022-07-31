cffdce4029e0d5c9b5b152b088a80e19af21090a
SnehaSingarapu <110330200+SnehaSingarapu@users.noreply.github.com>
2022-07-31 16:00:31 +0530

    Add files via upload

---
+++ Firsttest.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import pandas as pd
+
+
+# In[5]:
+
+
+data=pd.read_excel(r"C:\Users\Shiva Narayana\Desktop\Seshu_JAN Accounts.pdf.xlsx")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
